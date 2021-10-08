60bc52dc162f2817bc0198eed11166813c718227
kelvynking <kelvyn.m.king@gmail.com>
2021-10-08 12:05:52 +1100

    Created a function writeCards which takes in 2 arguments and returns an array of thank you messages for each name provided.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+function writeCards(arr, event) {
+    const thankYouCards = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        
+        const message = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`; 
+
+        thankYouCards.push(message);
+    }
+
+    return thankYouCards;
+}
